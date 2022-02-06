0b66178642982d142285a5c92ed6b8240e702d5d
Arman Bilge <armanbilge@gmail.com>
2022-02-06 07:05:21 +0000

    Formatting

--- hpack/src/main/scala/org/http4s/hpack/HuffmanDecoder.scala
+++ hpack/src/main/scala/org/http4s/hpack/HuffmanDecoder.scala
@@ -41,28 +41,25 @@ final class HuffmanDecoder(root: Node) {
   private[this] val EOS_DECODED = new IOException("EOS Decoded");
   private[this] val INVALID_PADDING = new IOException("Invalid Padding");
 
-  /**
-   * Creates a new Huffman decoder with the specified Huffman coding.
-   * @param codes   the Huffman codes indexed by symbol
-   * @param lengths the length of each Huffman code
-   */
-  def this(codes: Array[Int], lengths: Array[Byte]) {
+  /** Creates a new Huffman decoder with the specified Huffman coding.
+    * @param codes   the Huffman codes indexed by symbol
+    * @param lengths the length of each Huffman code
+    */
+  def this(codes: Array[Int], lengths: Array[Byte]) =
     this {
       if (codes.length != 257 || codes.length != lengths.length) {
         throw new IllegalArgumentException("invalid Huffman coding");
       }
       buildTree(codes, lengths);
     }
-  }
 
-  /**
-   * Decompresses the given Huffman coded string literal.
-   * @param  buf the string literal to be decoded
-   * @return the output stream for the compressed data
-   * @throws IOException if an I/O error occurs. In particular,
-   *         an <code>IOException</code> may be thrown if the
-   *         output stream has been closed.
-   */
+  /** Decompresses the given Huffman coded string literal.
+    * @param  buf the string literal to be decoded
+    * @return the output stream for the compressed data
+    * @throws IOException if an I/O error occurs. In particular,
+    *         an <code>IOException</code> may be thrown if the
+    *         output stream has been closed.
+    */
   @throws[IOException]
   def decode(buf: Array[Byte]): Array[Byte] = {
     val baos = new ByteArrayOutputStream();
@@ -72,11 +69,11 @@ final class HuffmanDecoder(root: Node) {
     var bits = 0;
     var i = 0
     while (i < buf.length) {
-      val b = buf(i) & 0xFF;
+      val b = buf(i) & 0xff;
       current = (current << 8) | b;
       bits += 8;
       while (bits >= 8) {
-        val c = (current >>> (bits - 8)) & 0xFF;
+        val c = (current >>> (bits - 8)) & 0xff;
         node = node.children(c);
         bits -= node.bits;
         if (node.isTerminal()) {
@@ -92,7 +89,7 @@ final class HuffmanDecoder(root: Node) {
 
     var break = false
     while (!break && bits > 0) {
-      val c = (current << (8 - bits)) & 0xFF;
+      val c = (current << (8 - bits)) & 0xff;
       node = node.children(c);
       if (node.isTerminal() && node.bits <= bits) {
         bits -= node.bits;
@@ -119,31 +116,27 @@ final class HuffmanDecoder(root: Node) {
 object HuffmanDecoder {
 
   private final class Node(
-    val symbol: Int, // terminal nodes have a symbol
-    val bits: Int, // number of bits matched by the node
-    val children: Array[Node] // internal nodes have children
+      val symbol: Int, // terminal nodes have a symbol
+      val bits: Int, // number of bits matched by the node
+      val children: Array[Node], // internal nodes have children
   ) {
 
-    /**
-     * Construct an internal node
-     */
-    private[HuffmanDecoder] def this() {
+    /** Construct an internal node
+      */
+    private[HuffmanDecoder] def this() =
       this(0, 8, new Array[Node](256))
-    }
 
-    /**
-     * Construct a terminal node
-     * @param symbol the symbol the node represents
-     * @param bits   the number of bits matched by this node
-     */
+    /** Construct a terminal node
+      * @param symbol the symbol the node represents
+      * @param bits   the number of bits matched by this node
+      */
     private[HuffmanDecoder] def this(symbol: Int, bits: Int) {
       this(symbol, bits, null)
       assert(bits > 0 && bits <= 8);
     }
 
-    private[HuffmanDecoder] def isTerminal(): Boolean = {
+    private[HuffmanDecoder] def isTerminal(): Boolean =
       return children == null;
-    }
   }
 
   private def buildTree(codes: Array[Int], lengths: Array[Byte]): Node = {
@@ -165,7 +158,7 @@ object HuffmanDecoder {
         throw new IllegalStateException("invalid Huffman code: prefix not unique");
       }
       length = (length - 8).toByte;
-      val i = (code >>> length) & 0xFF;
+      val i = (code >>> length) & 0xff;
       if (current.children(i) == null) {
         current.children(i) = new Node();
       }
@@ -174,12 +167,12 @@ object HuffmanDecoder {
 
     val terminal = new Node(symbol, length);
     val shift = 8 - length;
-    val start = (code << shift) & 0xFF;
+    val start = (code << shift) & 0xff;
     val end = 1 << shift;
     var i = start
     while (i < start + end) {
       current.children(i) = terminal;
       i += 1
     }
-  }  
+  }
 }
